ed1eb71a1b5b1f0c9471dd357ce3a6d4a3f12442
remy <remy.sucre@gmail.com>
2015-09-12 15:30:38 -0400

    DATAPOINTS

---
+++ fullparse/fpar.hs
@@ -0,0 +1,13 @@
+import System.Environment
+import Data.List
+
+bitv :: String -> [Double]
+bitv f = map (read . last) rows
+  where rows = map words $ drop 3 $ lines f
+
+timev = sort . bitv
+
+main = do
+  [fp] <- getArgs
+  fc <- readFile fp
+  putStr $ unlines $ map show (bitv fc)
